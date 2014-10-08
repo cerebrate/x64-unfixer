3f94f8bd7476617910eb4e1243d0dfb210a4abc4
cerebrate <avatar@arkane-systems.net>
2014-10-08 17:55:40 -0500

    Updated to handle a more broad spectrum of bans, seeing as those exist now.

--- x64-unfixer/Program.cs
+++ x64-unfixer/Program.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 using Mono.Cecil;
 using Mono.Cecil.Cil;
@@ -39,61 +36,80 @@ namespace Arkane.KSP.X64Unfixer
                 return;
             }
 
-            Console.WriteLine("Assembly loaded: {0}...", assembly.Name);
+            Console.WriteLine ("Assembly loaded: {0}...", assembly.Name);
 
-            // Find the CompatibilityChecker type.
-            TypeDefinition cc;
-
-            try
-            {
-                cc = assembly.MainModule.Types.Single (td => td.Name == "CompatibilityChecker");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Could not find CompatibilityChecker: {0}.", ex.Message);
-                return;
-            }
-
-            Console.WriteLine("...found CompatibilityChecker...");
-
-            // Find the IsWin64 method.
-            MethodDefinition isW64;
+            // See, I could just have disabled that one function and everything would have been swell.
+            // But you weren't going to let me do that, were you?
+            // So now I have to do this brute-force crap instead.
+            // For science. You monster.
 
             try
             {
-                isW64 = cc.Methods.Single (md => md.Name == "IsWin64");
+
+                ModuleDefinition corlib = ModuleDefinition.ReadModule (typeof (object).Module.FullyQualifiedName);
+                TypeDefinition intDefinition = corlib.GetType ("System.Int32");
+                TypeDefinition intptrDefinition = corlib.GetType ("System.IntPtr");
+                PropertyDefinition sizeDefinition = intptrDefinition.Properties.Single (pd => pd.Name == "Size");
+                MethodDefinition getDefinition = sizeDefinition.GetMethod;
+
+                // Iterate through every single type in the module.
+                foreach (var td in assembly.MainModule.Types)
+                {
+                    Console.WriteLine ("Examining type: {0}", td.Name);
+
+                    bool updatedMethod = false;
+
+                    // Iterate through every single method in the type.
+                    // This includes constructors, property implementors, etc.
+                    foreach (var md in td.Methods)
+                    {
+                        Console.WriteLine ("  Examining method: {0}", md.Name);
+
+                        // Examine their IL.
+                        var ilp = md.Body.GetILProcessor ();
+
+                        List<Instruction> toReplace = new List<Instruction> ();
+
+                        foreach (Instruction fe in ilp.Body.Instructions)
+                        {
+                            if ((fe.OpCode == OpCodes.Call) &&
+                                (fe.Operand != null) &&
+                                (((MethodReference) fe.Operand).Name == "get_Size"))
+                            {
+                                Console.WriteLine ("    System.IntPtr reference found, replacing...");
+
+                                toReplace.Add(fe);
+
+                                assembly.MainModule.Import (md);
+                                updatedMethod = true;
+                            }
+                        }
+
+                        foreach (Instruction fe in toReplace)
+                        {
+                            ilp.Replace (fe, Instruction.Create (OpCodes.Ldc_I4_4));
+                        }
+                    }
+
+                    // If we updated the method, we must also update the type.
+                    if (updatedMethod)
+                        assembly.MainModule.Import (td);
+                }
+
+                // So sorry, cleverer than that.
+
+                File.Move (filepath, oldpath);
+
+                assembly.Write (filepath);
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Could not find IsWin64() method: {0}", ex.Message);
-                return;
-            }    
-            
-            Console.WriteLine("...found IsWin64()...");
-
-            // Patch it to always return false.
-            try
-            {
-                var ilp = isW64.Body.GetILProcessor ();
-
-                var first = ilp.Body.Instructions.First ();
-
-                ilp.InsertBefore (first, ilp.Create(OpCodes.Ldc_I4_0));
-                ilp.InsertBefore (first, ilp.Create (OpCodes.Ret));
-
-                assembly.MainModule.Import (isW64);
-                assembly.MainModule.Import (cc);
-
-                File.Move(filepath, oldpath);
-
-                assembly.Write(filepath);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error rewriting assembly: {0}", ex.Message);
+                Console.WriteLine ("Error rewriting assembly: {0}", ex.Message);
             }
 
             Console.WriteLine ("Operation completed.");
+
+            Console.ReadLine ();
         }
     }
 }
